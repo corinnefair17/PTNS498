7f248e62a0f6d29047918d75b77414554b7989f4
Corinne Fair <corinne.fair.17@cnu.edu>
2021-03-08 23:15:21 -0500

    Starting new repo because I didn't do the first one right. Created first dungeon layout with spritesheet & added collisions

---
+++ PTNS 498/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Handles collisions and physics for 2D characters
+    Rigidbody2D body;
+
+    // Track which keys are being pressed on the keyboard
+    float horizontal;
+    float vertical;
+
+    // Controls movement speed
+    public float movementSpeed = 20.0f;
+
+    // A variable to be used to slow down diagonal movement
+    float moveLimiter = 0.7f;
+
+    // Initialize body and attach it to player sprite
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Updates upon each frame load
+    void Update()
+    {
+        // These values will return either 1 or -1 for direction
+        // 0 is no movement
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    // Updates whenever physics calculations are needed
+    void FixedUpdate()
+    {
+        // Prevent diagonal movement from being too fast
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        // Set the player's movement to input direction at given movement speed
+        body.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
+    }
+}
---
+++ PTNS 498/Assets/Scripts/RollDice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollDice : MonoBehaviour
+{
+    public int roll(int numSides, int numDice, int mod)
+    {
+        System.Random r = new System.Random();
+        int total = 0;
+        for (int i = 0; i < numDice; i++)
+        {
+            total += r.Next(1, numSides) + mod;
+        }
+        return total;
+    }
+}
