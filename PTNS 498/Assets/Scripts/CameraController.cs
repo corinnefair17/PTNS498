a4c9d17a4b1002bfac557303d5ef36574743f543
Corinne Fair <corinne.fair.17@cnu.edu>
2021-03-10 23:53:29 -0500

    finished dungeon and player sprite

--- PTNS 498/Assets/Scripts/CameraController.cs
+++ PTNS 498/Assets/Scripts/CameraController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-
     private Vector3 offset;
 
     // Start is called before the first frame update
